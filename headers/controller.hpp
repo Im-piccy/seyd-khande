d809f56050f333ac05a78426f666f8f6aa48d3a7
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 10:58:01 -0700

    added a memeber function named manage_chosing_character_screen_logic

--- headers/controller.hpp
+++ headers/controller.hpp
@@ -8,6 +8,8 @@ class Controller
         void execute_user_ask_to_use_hero_ability_if_possible(); //it gets user request to atteck and does the logic (considering energy cost)
         void Refill_Players_Energy(); // after each round ends it refill energies accordingly
         void Finish_Round(); // this function increments the private round_number variable by one
+        void manage_chosing_character_screen_logic();//this function instantiates objects and let users pick their heros
+        
     private:
         int round_number; // to keep track of the current game round
         int which_user_started_the_game; // to remember which user started first
