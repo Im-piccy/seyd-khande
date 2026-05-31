d98eafcb609628816ed20ae3510fe36581f70e0f
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 10:20:39 -0700

    wrote a prototype for user class

---
+++ headers/user.hpp
@@ -0,0 +1,15 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+class User
+{
+    public:
+        void call_hero_abillity(); //user decides which hero's ability to use and it tells the controller object
+        void terminate_user_turn_manually(); //this means that user is skiping turn or ending attacks before energy is 0
+        int return_currnet_energy();
+    private:
+        int Hero_Arr[3];// this shows which heros the user possess
+        int energy;
+};
+
+#endif
