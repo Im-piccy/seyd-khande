94e7bf64fe70c82834b7441b24220f15acf46dd5
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 09:50:34 -0700

    wrote a prototype for controller class

---
+++ headers/controller.hpp
@@ -0,0 +1,16 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+class Controller
+{
+    public:
+        bool Is_game_over(); // checks to see if game raound is 15 or one players heros are all dead
+        void Attack(); //it gets user request to atteck and does the logic (considering energy cost)
+        void Refill_Players_Energy(); // after each round ends it refill energies accordingly
+        void Finish_Round(); // this function increments the private round_number variable by one
+    private:
+        int round_number; // to keep track of the current game round
+        int which_user_started_the_game; // to remember which user started first
+};
+
+#endif
