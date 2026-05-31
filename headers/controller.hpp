e1584811f8ad666bc42d2c5a584f6f99181a60f9
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 10:20:07 -0700

    wrote a prototype for controller class

--- headers/controller.hpp
+++ headers/controller.hpp
@@ -5,7 +5,7 @@ class Controller
 {
     public:
         bool Is_game_over(); // checks to see if game raound is 15 or one players heros are all dead
-        void Attack(); //it gets user request to atteck and does the logic (considering energy cost)
+        void execute_user_ask_to_use_hero_ability_if_possible(); //it gets user request to atteck and does the logic (considering energy cost)
         void Refill_Players_Energy(); // after each round ends it refill energies accordingly
         void Finish_Round(); // this function increments the private round_number variable by one
     private:
