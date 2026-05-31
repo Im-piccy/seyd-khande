1bef0ff515b6c1c42e54c0a36801399ef7e67ac3
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 16:20:22 -0700

    added a game controller class

---
+++ headers/game.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+enum SCREENS{MENU_SCREEN, CHARACTER_SELECT_SCREEN, GAME_SCREEN, GAME_OVER_SCREEN};
+
+//this class actually runs the game and ties everything together
+
+class Game
+{
+    public:    
+        void Manage_Screens();
+        void Menu_Screen();
+        void Character_Select_Screen();
+        void Game_Screen();
+        void Game_Over_Screen();
+    private:
+    
+};
+
+#endif
