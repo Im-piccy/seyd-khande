1a76929185509c54a5400214dc6da4ffdc7aee40
farzadNobahar <farzadpiczol22@gmail.com>
2026-05-31 11:28:49 -0700

    wrote a prototype for taha kochike hero

--- headers/hero_sub_class_to_instatniate_objects.hpp
+++ headers/hero_sub_class_to_instatniate_objects.hpp
@@ -8,11 +8,21 @@ class WhiteDoctor : public Hero_Abstaction
         WhiteDoctor();//this constructor should initilize heros info like hp, superpower cost ...
         bool Execute_Asprin_Ability(int current_energy);//return type is bool that means if there is not enaugh energy for the attack it will return false
         bool Execute_Doping_Ability(int current_energy);
-        bool Execute_SuperPower(int current_energy);
     private:
         int Asprin_Ability_Energy_Cost;
         int Doping_Ability_Energy_Cost;
         
+};
+
+class Taha_Kochike
+{
+    public:
+        Taha_Kochike(); // constructor to initilize the hero information
+        bool Execute_Tigh_Tiz_Ability(int current_energy); 
+        bool Execute_Serom_Khon_Ability(int current_energy); 
+    private:
+        int Tigh_Tiz_Ability_Energy_Cost;
+        int Serom_Khon_Ability_Energy_Cost;
 };
 
 
