bcbdcf05b2322d2e15552ba659234f6c080aa21c
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-02 16:08:43 -0700

    friended the print pose funtion in game class with the user class

--- headers/user.hpp
+++ headers/user.hpp
@@ -1,8 +1,10 @@
 #ifndef USER_HPP
 #define USER_HPP
+#include "game.hpp"
 class Hero_Abstaction;
 class User
 {
+    friend void Game::print_characters_gaurd_pose_on_screen_according_to_user_array();
     public:
         void call_hero_abillity(); //user decides which hero's ability to use and it tells the controller object
         void terminate_user_turn_manually(); //this means that user is skiping turn or ending attacks before energy is 0
