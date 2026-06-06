e387f6e9d64fc5efd5ca60982fa92a9dc98e1618
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-06 16:24:03 -0700

    added some memeber functions and in the private part i added some texture variables so that i could easily access it when needed in the game screen member funtion because if i wouldnt have done that i should have written all the code in that function and it would have caused it to be a mess

--- headers/game.hpp
+++ headers/game.hpp
@@ -2,6 +2,7 @@
 #define GAME_HPP
 #include "controller.hpp"
 #include "user.hpp"
+#include <raylib.h>
 enum SCREENS{MENU_SCREEN, CHARACTER_SELECT_SCREEN, GAME_SCREEN, GAME_OVER_SCREEN, WINDOWSHOULDCLOSE};
 enum TURN{USER1, USER2};
 
@@ -9,6 +10,7 @@ enum TURN{USER1, USER2};
 
 class Game
 {
+    
     public:    
         Game();
         void print_characters_gaurd_pose_on_screen_according_to_user_array();
@@ -16,12 +18,24 @@ class Game
         void Menu_Screen();
         void Character_Select_Screen();
         void Game_Screen();
+        void load_animation_sprite_sheets();
+        void unload_animation_sprite_sheets();
+        void print_heros_on_screen_idle_and_highlight_hero_which_is_hoverd(const std::array <int,3>& user1_heros, const std::array <int,3>&  user2_heros, int index, int user_turn,bool should_highlight,int hero_being_hoverd);  
+        Texture2D return_texture_based_on_arguments_passed(const std::array <int,3>& user1_hero_arr, const std::array <int,3>& user2_hero_arr,int user_turn, int hero_index_in_array);
         //bool Game_Over_Screen();
     private:
         int current_screen;
         User user1;
         User user2;
         Controller control;
+
+        Texture2D Doc_anim_texture;
+        Texture2D Dani_anim_texture;
+        Texture2D Amin_anim_texture; 
+        Texture2D Tlittle_anim_texture;
+        Texture2D Tbig_anim_texture;
+        Texture2D Pouya_anim_texture;
+        Texture2D Shahriar_anim_texture;
 };
 
 #endif
